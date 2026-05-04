8b6052d1c6229297fe7504328b8f4df405750b66
Jonathan Wagner <dustyhair@gmail.com>
2026-05-04 00:19:16 +0000

    rootnex: remove TU102 QINV breadcrumbs

--- usr/src/uts/i86pc/io/immu_qinv.c
+++ usr/src/uts/i86pc/io/immu_qinv.c
@@ -856,37 +856,19 @@ void
 immu_qinv_intr_one_cache(immu_t *immu, uint_t iidx, immu_inv_wait_t *iwp)
 {
 	if (immu_qinv_haswell_quirk && immu_qinv_haswell_force_global_iec) {
-		prom_printf("QINV-TU102: one-cache global-start idx=%u\n",
-		    iidx);
 		immu_qinv_intr_trace_record(immu, 'g', 's', 0, 0, iwp);
-		prom_printf("QINV-TU102: one-cache global-post-trace "
-		    "idx=%u\n", iidx);
 		qinv_iec_common(immu, 0, 0, IEC_INV_GLOBAL);
-		prom_printf("QINV-TU102: one-cache global-post-iec idx=%u\n",
-		    iidx);
 		qinv_wait_sync(immu, iwp);
-		prom_printf("QINV-TU102: one-cache global-post-wait "
-		    "idx=%u\n", iidx);
 		immu_qinv_intr_postsync(immu);
-		prom_printf("QINV-TU102: one-cache global-post-sync "
-		    "idx=%u\n", iidx);
 		immu_qinv_intr_trace_record(immu, 'g', 'c', 0, 0, iwp);
-		prom_printf("QINV-TU102: one-cache global-done idx=%u\n",
-		    iidx);
 		return;
 	}
 
-	prom_printf("QINV-TU102: one-cache start idx=%u\n", iidx);
 	immu_qinv_intr_trace_record(immu, 'o', 's', iidx, 1, iwp);
-	prom_printf("QINV-TU102: one-cache post-trace idx=%u\n", iidx);
 	qinv_iec_common(immu, iidx, 0, IEC_INV_INDEX);
-	prom_printf("QINV-TU102: one-cache post-iec idx=%u\n", iidx);
 	qinv_wait_sync(immu, iwp);
-	prom_printf("QINV-TU102: one-cache post-wait idx=%u\n", iidx);
 	immu_qinv_intr_postsync(immu);
-	prom_printf("QINV-TU102: one-cache post-sync idx=%u\n", iidx);
 	immu_qinv_intr_trace_record(immu, 'o', 'c', iidx, 1, iwp);
-	prom_printf("QINV-TU102: one-cache done idx=%u\n", iidx);
 }
 
 /* queued invalidation interface -- invalidate interrupt entry caches */
